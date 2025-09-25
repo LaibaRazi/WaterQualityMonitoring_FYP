456871979c315705fc1f279146e2800f4cb09aad
LaibaRazi <khanlayba72@gmail.com>
2025-09-26 02:03:03 +0500

    made android works n changed the versionin gradle files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android") // use the Kotlin plugin defined in settings.gradle.kts
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id 'com.google.gms.google-services' version '4.4.3' apply false
+    id("com.google.gms.google-services") // version controlled in settings.gradle.kts
 }
-
 
 android {
     namespace = "com.example.waterqualitymonitoring"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -18,24 +17,19 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11"
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.waterqualitymonitoring"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        minSdk = 23
+        targetSdk = 34
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -44,14 +38,12 @@ android {
 flutter {
     source = "../.."
 }
+
 dependencies {
-    // Import the Firebase BoM
-    implementation platform('com.google.firebase:firebase-bom:34.0.0')
-    // TODO: Add the dependencies for Firebase products you want to use
-    // When using the BoM, don't specify versions in Firebase dependencies
-    implementation 'com.google.firebase:firebase-analytics'
+    // Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
-
-    // Add the dependencies for any other desired Firebase products
+    // Add other Firebase products as needed
     // https://firebase.google.com/docs/android/setup#available-libraries
 }
